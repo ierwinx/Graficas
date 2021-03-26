70c19ad462c1ec6230b2b0cc2028a2a9a56bfbbd
Erwin <ierwinx@gmail.com>
2021-03-26 13:32:14 -0600

    se inicializa el proyecto

--- Graficas1/ViewController.swift
+++ Graficas1/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  Graficas1
-//
-//  Created by Erwin on 26/03/21.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
-
